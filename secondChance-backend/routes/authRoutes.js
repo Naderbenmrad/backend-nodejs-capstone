72a1a8e3be7162bdb038153acfd6676e2733583e
naderbenmrad <nader.benmrad@gmail.com>
2025-04-30 18:03:38 -0400

    Develop Back-end Integration for Login

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -4,17 +4,22 @@ const jwt = require('jsonwebtoken');
 const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
-const pino = require('pino');
+const pino = require('pino'); 
 dotenv.config();
 
-const logger = pino();
+const logger = pino(); 
 
+dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
     try {
+
       const db = await connectToDatabase();
+
       const collection = db.collection("users");
+
+
       const existingEmail = await collection.findOne({ email: req.body.email });
 
         if (existingEmail) {
@@ -25,6 +30,7 @@ router.post('/register', async (req, res) => {
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email=req.body.email;
+
         const newUser = await collection.insertOne({
             email: req.body.email,
             firstName: req.body.firstName,
@@ -46,6 +52,40 @@ router.post('/register', async (req, res) => {
         logger.error(e);
         return res.status(500).send('Internal server error');
     }
+});
+
+router.post('/login', async (req, res) => {
+    console.log("\n\n Inside login")
+
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+        const theUser = await collection.findOne({ email: req.body.email });
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            if(!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+            logger.info('User logged in successfully');
+            return res.status(200).json({ authtoken, userName, userEmail });
+        } else {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).json({ error: 'Internal server error', details: e.message });
+      }
 });
 
 module.exports = router;
