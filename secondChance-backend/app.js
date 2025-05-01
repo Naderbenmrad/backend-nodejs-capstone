53e661fc922662d400aa94f9436900a8e87a4dd1
naderbenmrad <nader.benmrad@gmail.com>
2025-05-01 13:25:22 +0100

    Integrate Backend to the Frontend 3

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -5,17 +5,16 @@ const pinoLogger = require('./logger')
 const path = require('path')
 
 const connectToDatabase = require('./models/db')
-//const { loadData } = require('./util/import-mongo/index ')
+// const { loadData } = require('./util/import-mongo/index ')
 
 const app = express()
-app.use('*',cors())
+app.use('*', cors())
 const port = 3060
 
 connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
+  pinoLogger.info('Connected to DB')
 })
-    .catch((e) => console.error('Failed to connect to DB', e));
-
+    .catch((e) => console.error('Failed to connect to DB', e))
 
 app.use(express.json())
 
@@ -33,9 +32,9 @@ app.use('/api/auth', authRoutes)
 app.use('/api/secondchance/search', searchRoutes)
 
 app.use((err, req, res, next) => {
-    console.error(err)
-    res.status(500).send('Internal Server Error')
-});
+  console.error(err)
+  res.status(500).send('Internal Server Error')
+})
 
 app.get("/",(req,res)=>{
     res.send("Inside the server")
